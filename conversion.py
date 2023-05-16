8d3869d5538d504b572e1cbc89e061c3859e5f09
ole <gitlab+account@ole.mn>
2023-05-16 12:31:02 +0200

    add documentation

--- conversion.py
+++ conversion.py
@@ -1,3 +1,10 @@
 def celsius_to_kelvin(celsius):
-    """Function to return Kelvin degrees from Celsius input"""
+    """Function to return Kelvin degrees from Celsius input
+
+    Args:
+        celsius (float): Temperature in degrees Celsius
+
+    Returns:
+        The temperature in Kelvin (float)
+    """
     return celsius + 273.15
